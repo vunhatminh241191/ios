2b87e95a77d33c30d7f38a148f5a7208261c4038
Huy <huy8@pdx.edu>
2015-11-04 10:33:03 -0800

    push to new branch

--- eber_ios/SignInViewController.swift
+++ eber_ios/SignInViewController.swift
@@ -28,10 +28,7 @@ class SignInViewController: UIViewController, FBSDKLoginButtonDelegate {
         else{
             print("invaled user")
         }
-<<<<<<< HEAD
         
-=======
->>>>>>> e0000587d97bffae485c9db2e5d046279d436166
     }
     
     override func viewDidLoad() {
